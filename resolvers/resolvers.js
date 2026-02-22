dedb7317aeffd2672df918f4a4b60efb17ccdd0a
Sokmontrey <sythatsokmontrey@gmail.com>
2026-02-22 14:52:26 -0500

    Add movie query resolvers for fetching all movies, a single movie by ID, and movies by director

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -2,7 +2,17 @@ import MovieModel from '../models/Movie.js';
 
 // Resolvers define the technique for fetching the types defined in the schema.
 const movieResolvers = {
-  Query: {},
+  Query: {
+    movies: async () => {
+      return await MovieModel.find();
+    },
+    movie: async (_, { id }) => {
+      return await MovieModel.findById(id);
+    },
+    moviesByDirector: async (_, { director_name }) => {
+      return await MovieModel.findByDirector(director_name);
+    },
+  },
   Mutation: {},
 };
 
